65a43fcba746fd523e4c5d65cda27e1e7c7da439
joca <jpereyraleon@gmail.com>
2016-06-29 19:24:12 -0500

    change # by id

--- app/Http/Controllers/Telegram/ResponseController.php
+++ app/Http/Controllers/Telegram/ResponseController.php
@@ -51,7 +51,7 @@ class ResponseController extends AbstractTelegramController
         if ($tracks->isEmpty()) return $this->telegram->replyKeyboardHide();
 
         $keyboard = $tracks->take(3)->map(function ($track) {
-            return ['#'.$track['track_id'].' - '.$track['track_name'].' - '.$track['artist_name']];
+            return ['id:'.$track['track_id'].' - '.$track['track_name'].' - '.$track['artist_name']];
         });
 
         return $this->telegram->replyKeyboardMarkup([
